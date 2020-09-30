72f9581023400aa3fee34cd100932aeb0554a932
Jonathan Bowman <git@bowmanjd.com>
2020-09-30 18:53:06 -0400

    fix: removed old chevron code

--- pytemplates/engines/chevron/config.py
+++ pytemplates/engines/chevron/config.py
@@ -6,22 +6,6 @@ INCLUDES_RE = r"{{>\s+(\S+)\s*}}"
 INCLUDES_EXT = ".mustache"
 
 
-def setup():
-    """Initial environment setup."""
-    config = {}
-    config["path"] = Path("pytemplates/engines/chevron/")
-    return config
-
-
-def render(config, template_name, variables):
-    """Render template with interpolated variables."""
-    with (config["path"] / template_name).open() as template:
-        rendered = chevron.render(
-            template, variables, partials_path=str(config["path"])
-        )
-    return rendered
-
-
 def compile_template(template_dict, template_name):
     """Compile template."""
     compiled = {
@@ -29,24 +13,6 @@ def compile_template(template_dict, template_name):
         for template_name in template_dict
     }
     return compiled, template_name
-
-
-# def compile_template(template, template_path, partials_dict=None):
-#     """Compile template."""
-#     if partials_dict is None:
-#         partials_dict = {}
-#     new_partials_dict = partials_dict.copy()
-#     tokenized = tuple(chevron.tokenizer.tokenize(template))
-#     for tag in tokenized:
-#         if tag[0] == "partial" and tag[1] not in new_partials_dict:
-#             partial_template = template_path.joinpath(f"{tag[1]}.mustache").read_text()
-#             partial, partial_partials_dict = compile_template(
-#                 partial_template, template_path, new_partials_dict
-#             )
-#             new_partials_dict[tag[1]] = partial
-#             new_partials_dict = {**new_partials_dict, **partial_partials_dict}
-#
-#     return (tokenized, new_partials_dict)
 
 
 def render_compiled(compiled, variables):
