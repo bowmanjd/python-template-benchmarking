0e01f5714aa5b12eed4a0143e0398f57cf0cb354
Jonathan Bowman <git@bowmanjd.com>
2020-10-02 07:46:43 -0400

    fix: refactored liquidpy with include instead of extends

--- pytemplates/engines/liquidpy/config.py
+++ pytemplates/engines/liquidpy/config.py
@@ -1,17 +1,26 @@
 """Rendering for Liquidpy engine."""
 
-from pathlib import Path
 
 from liquid import Liquid
 
-
-def setup():
-    """Initial environment setup."""
-    config = {"path": Path("pytemplates/engines/liquidpy/")}
-    return config
+INCLUDES_RE = r"{%\s*(?:extends|include)\s+['\"]([^'\"]+)['\"]\s*%}"
 
 
-def render(config, template_name, variables):
-    """Render template with interpolated variables."""
-    template = Liquid(str(config["path"] / template_name), liquid_from_file=True)
+def compile_template(template_dict, template_name):
+    """Compile template."""
+    template_path = "pytemplates/engines/liquidpy/"
+    template = Liquid(
+        template_dict[template_name], liquid_config={"include_dir": [template_path]},
+    )
+    return template
+
+
+def render_compiled(compiled, variables):
+    """Render from compiled template with interpolated variables."""
+    return compiled.render(**variables)
+
+
+def render_from_file(template_file, variables):
+    """Render from template string with interpolated variables."""
+    template = Liquid(str(template_file))
     return template.render(**variables)
